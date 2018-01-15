c8b96723fde8bfa82dc361999256d324ad62c095
Adrien Saladin <adrien.saladin@gmail.com>
2018-01-15 10:30:02 +0100

    read kafka timestamps

--- src/main/java/com/pinterest/secor/parser/KafkaTimestampedMessageParser.java
+++ src/main/java/com/pinterest/secor/parser/KafkaTimestampedMessageParser.java
@@ -1,18 +1,62 @@
 package com.pinterest.secor.parser;
 
+import com.pinterest.secor.common.FileRegistry;
 import com.pinterest.secor.common.SecorConfig;
 import com.pinterest.secor.message.Message;
 
-public class KafkaTimestampedMessageParser extends TimestampedMessageParser {
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+public class KafkaTimestampedMessageParser extends DailyOffsetMessageParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaTimestampedMessageParser.class);
 
     public KafkaTimestampedMessageParser(SecorConfig config) {
+
         super(config);
+
+        LOG.error("coucou");
     }
+
+
+    @Override
+    public String[] extractPartitions(Message message) throws Exception {
+
+        long millis = toMillis(message.getTimestamp());
+        long offset = message.getOffset();
+        long offsetsPerPartition = mConfig.getOffsetsPerPartition();
+        long partition = (offset / offsetsPerPartition) * offsetsPerPartition;
+        String[] dailyPartition = generatePartitions(millis, mUsingHourly, mUsingMinutely);
+        String dailyPartitionPath = StringUtils.join(dailyPartition, '/');
+        //LOG.warn("parition path: {}", dailyPartitionPath);
+        String[] result = {dailyPartitionPath, offsetPrefix + partition};
+        return result;
+    }
+
 
     @Override
     public long extractTimestampMillis(final Message message) {
-        return toMillis(message.getTimestamp());
+
+        long millis = toMillis(message.getTimestamp());
+        LOG.error("date milli: {}", millis);
+        System.err.println("Prntln date milli: {}" + String.valueOf(millis));
+        try {
+        FileWriter fstream = new FileWriter("/tmp/as_secorout.txt", true);
+        BufferedWriter out = new BufferedWriter(fstream);
+        out.write("Prntln date milli: {}\n" + String.valueOf(millis));
+        }
+        catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        return millis;
+
     }
 
 
